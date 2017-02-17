6e4d1e57b59cd7961a18d3e7ff6550648928184e
lzx2005 <crow2005@vip.qq.com>
2017-02-17 11:22:55 +0800

    开始往分布式爬虫走

--- src/main/resources/sql/website_crawler.sql
+++ src/main/resources/sql/website_crawler.sql
@@ -1,15 +1,26 @@
-﻿DROP TABLE IF EXISTS `thread_mark`;
+﻿
+DROP TABLE IF EXISTS `thread_mark`;
 CREATE TABLE `thread_mark` (
-  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `mark_id` int(11) NOT NULL AUTO_INCREMENT,
   `host` varchar(255) NOT NULL DEFAULT '',
   `thread_name` varchar(255) NOT NULL DEFAULT '',
   `thread_id` varchar(255) NOT NULL DEFAULT '',
-  `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '0:Readly,1:Running,2:Done',
+  `status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '0:Readly,1:Running,2:Done,3:Failed',
   `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '创建时间',
   `start_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '开始时间',
   `done_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '完成时间',
-  PRIMARY KEY (`Id`)
+  PRIMARY KEY (`mark_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='线程记录器';
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `Id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
+  `password` varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
+  `user_type` tinyint(3) NOT NULL DEFAULT '1' COMMENT '0:管理员,1:普通用户',
+  `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT '注册时间',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户表';
 
 DROP TABLE IF EXISTS `website`;
 CREATE TABLE `website` (
@@ -19,8 +30,8 @@ CREATE TABLE `website` (
   `title` varchar(255) NOT NULL DEFAULT '',
   `head` text NOT NULL,
   `body` text NOT NULL,
-  `content` text NOT NULL COMMENT '正文内容',
-  `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `content` text NOT NULL COMMENT 'æ­£æ–‡å†…å®¹',
+  `create_time` datetime NOT NULL,
   `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`website_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7341 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=22694 DEFAULT CHARSET=utf8;
